33027bce16e9a016d87b583557363d8d378b6990
Filip Maciejowski <filipmaciejowski@Filips-MacBook-Pro.local>
2022-04-27 15:18:16 +0200

    redux established

---
+++ src/features/AdminsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  admins: [],
+};
+
+export const AdminsSlice = createSlice({
+  name: "admins",
+  initialState: initialState,
+  reducers: {
+    addAdmin: (state, action) => {
+      state.admins = [...state.admins, action.payload];
+    },
+    deleteAdmin: (state, action) => {
+      state.admins.splice(action.payload);
+    },
+  },
+});
+
+export const { addAdmin, deleteAdmin } = AdminsSlice.actions;
+
+export default AdminsSlice.reducer;
